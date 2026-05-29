59c0e4e4b794131cffa258405c8b28965442abda
Venkata Jayavardhan <nvjayavardhan@gmail.com>
2026-05-29 20:14:11 +0530

    added the chat part to backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 5000;
-app.use(express.json());
+const port = process.env.PORT || 5000;
+app.use(express.json({ limit: '10mb' }));
 const cors = require('cors'); // Import CORS middleware
 const connectToMongoDB = require('./db');
 
